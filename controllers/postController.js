c65d7768a5bc3dbd7698ba408d19fa5298c96be9
Divyanshu6797 <divyanshu6797@gmail.com>
2024-06-03 09:08:37 +0530

    edit post and delete post

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,7 @@
 const userModel = require("../models/userModel");
 const postModel = require("../models/postModel");
+const commentModel = require("../models/commentModel");
+const mongoose = require('mongoose');
 
 const addPost = async (req, res) => {
   console.log("req.body", req.body);
@@ -24,13 +26,11 @@ const addPost = async (req, res) => {
       postedBy: user.username,
     });
 
-    // Save the post to the database
     await newPost.save();
 
     // Populate the user field
     const populatedPost = await newPost.populate("user");
 
-    // Respond with the created post
     res.status(201).json(populatedPost);
   } catch (error) {
     console.error("Error creating post:", error);
@@ -44,7 +44,6 @@ const fetchAllPosts = async (req, res) => {
       .find({}, "id caption imageUrl postedBy")
       .sort({ updatedAt: -1 });
 
-    // Respond with the posts
     res.status(200).json(posts);
   } catch (error) {
     console.error("Error fetching posts", error);
@@ -52,25 +51,87 @@ const fetchAllPosts = async (req, res) => {
   }
 };
 
-const fetchUserPosts =  async (req, res) => {
-    const userId = req.userid
-    console.log(typeof userId, userId)
-    try {
-      const posts = await postModel
-        .find({user : userId}, "id caption imageUrl postedBy")
-        .sort({ updatedAt: -1 });
-  
-      // Respond with the posts
-      res.status(200).json(posts);
-    } catch (error) {
-      console.error("Error fetching posts", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
+const fetchUserPosts = async (req, res) => {
+  const userId = req.userid;
+  console.log(typeof userId, userId);
+  try {
+    const posts = await postModel
+      .find({ user: userId }, "id caption imageUrl postedBy")
+      .sort({ updatedAt: -1 });
+
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error("Error fetching posts", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
+
+const deletePostAndItsComments = async (req, res) => {{
+  
+  const { postId } = req.body;
+
+  if (!postId) {
+    return res.status(400).json({ error: 'Post ID is required' });
+  }
+
+  
+  if (!mongoose.Types.ObjectId.isValid(postId)) {
+    return res.status(400).json({ error: 'Invalid post ID' });
+  }
+
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    
+    const deletedPost = await postModel.findByIdAndDelete(postId).session(session);
+    if (!deletedPost) {
+      throw new Error('Post not found');
+    }
+
+    // Delete all comments associated with the post within the session
+    await commentModel.deleteMany({ post: postId }).session(session);
+
+    // Commit the transaction
+    await session.commitTransaction();
+    session.endSession();
+    console.log('Post and associated comments deleted successfully.');
+    return res.status(200).json({ message: 'Post and associated comments deleted successfully' , deletedPost});
+
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    console.error(`Error deleting post and associated comments: ${error}`);
+    return res.status(500).json({ error: error.message });
+  }
+}
+};
+
+const editPost = async (req, res) => {
+  console.log("hi")
+  const { postId, caption } = req.body;
+
+  try {
+    const updatedPost= await postModel.findByIdAndUpdate(
+      postId,
+      { caption },
+      { new: true }
+    );
+    if (!updatedPost) {
+      return res.status(404).json({ error: 'post not found' });
+    }
+
+    res.status(200).json({ message: 'post updated successfully', updatedPost });
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+
+}
 
 module.exports = {
   addPost,
   fetchAllPosts,
   fetchUserPosts,
- 
+  deletePostAndItsComments,
+  editPost
 };
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const {addPost, fetchAllPosts, fetchUserPosts } = require('../controllers/postController');
+const {addPost, fetchAllPosts, fetchUserPosts , deletePostAndItsComments, editPost} = require('../controllers/postController');
 
 const {postMiddle} = require('../middleware/postMiddleware');
 
@@ -15,6 +15,8 @@ router.use(
 router.post('/addpost', addPost);
 router.get('/allposts', fetchAllPosts);
 router.get('/userposts', fetchUserPosts);
+router.delete('/deletepostanditscomments', deletePostAndItsComments);
+router.put('/editpost', editPost);
 
 
 
